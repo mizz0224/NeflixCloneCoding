0dd1ddb60702b090d71b0608abbf9a9b7b3dbd21
mizz0224 <wosud0224@naver.com>
2022-07-17 19:36:35 +0900

    test
    ~ 1:48:47

--- Netflix CloneCoding/Controllers/HomeViewController.swift
+++ Netflix CloneCoding/Controllers/HomeViewController.swift
@@ -30,7 +30,7 @@ class HomeViewController: UIViewController  {
         let headerView = HeroHeaderUIVuew(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
         
-        getTrendingMovies()
+        fetchData()
     }
     private func configureNavbar(){
         var image = UIImage(named: "netflixLogo")
@@ -50,17 +50,20 @@ class HomeViewController: UIViewController  {
         homeFeedTable.frame = view.bounds
     }
     
-    private func getTrendingMovies(){
-        APICaller.shared.getTrendingMovies { results in
-            switch results {
-                
-            case.success(let movies):
-                print(movies)
-            case.failure(let error):
-                print(error)
-                
-            }
-        }
+    private func fetchData(){
+//        APICaller.shared.getTrendingMovies { results in
+//            switch results {
+//
+//            case.success(let movies):
+//                print(movies)
+//            case.failure(let error):
+//                print(error)
+//
+//            }
+//        }
+        APICaller.shared.getTrendingTvs { result in
+            
+        } 
     }
     
 }
---
+++ Netflix CloneCoding/Models/Tv.swift
@@ -0,0 +1,8 @@
+//
+//  Tv.swift
+//  Netflix CloneCoding
+//
+//  Created by heojaenyeong on 2022/07/17.
+//
+
+import Foundation
